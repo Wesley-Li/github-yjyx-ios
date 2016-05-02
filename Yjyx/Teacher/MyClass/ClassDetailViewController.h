a1096e2e950f5a2d050a62dbee2bfb7ef4a51db8
刘少昌 <liushaochang@liushaochangdeMacBook-Pro.local>
2016-05-03 00:27:52 +0800

    标签栏修改

---
+++ Yjyx/Teacher/MyClass/ClassDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClassDetailViewController.h
+//  Yjyx
+//
+//  Created by 刘少昌 on 16/4/28.
+//  Copyright © 2016年 Alibaba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClassDetailViewController : UIViewController
+
+@end
