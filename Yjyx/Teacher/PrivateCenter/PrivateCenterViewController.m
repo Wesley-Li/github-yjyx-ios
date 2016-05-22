0e21314b4dfe3acaaa8ba55a0fbc12db5f8fcd77
zhujianyu <zhujianyu@promote.cache-dns.local>
2016-05-23 01:46:09 +0800

    退出登录优化

--- Yjyx/Teacher/PrivateCenter/PrivateCenterViewController.m
+++ Yjyx/Teacher/PrivateCenter/PrivateCenterViewController.m
@@ -247,6 +247,13 @@
             
             if ([[result objectForKey:@"retcode"] integerValue] == 0) {
                 // 退出时关闭定时器,并清除用户名密码等信息
+                NSArray *array = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@",BaseURL,TEACHER_LOGIN_CONECT_POST]]];
+                for (NSHTTPCookie *cookie in array)
+                {
+                    [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+                }
+
+                
                 [SYS_CACHE removeObjectForKey:@"AutoLogoin"];
                 // 定时器挂起
                 dispatch_suspend(((AppDelegate*)SYS_DELEGATE).timer);
